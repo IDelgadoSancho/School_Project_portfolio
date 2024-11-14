ca5313ade777b01aa7fec0fae4377b0196e7b81a
Ismael Delgado Sancho <ismael_delgadosancho@iescarlesvallbona.cat>
2024-11-14 21:05:18 +0100

    landing_v1

--- js/code.js
+++ js/code.js
@@ -1,9 +1,14 @@
 window.onload = async function () {
-    let navItems = document.querySelectorAll(".nav ul li");
-    let delay = 320; // 200ms de retardo entre cada elemento
+  let navItems = document.querySelectorAll(".nav ul li");
+  let landing = document.getElementById("landing_div");
+  let footer = document.getElementById("footer");
+  let delay = 320; // 200ms de retardo entre cada elemento
 
-    for (let i = 0; i < navItems.length; i++) {
-        await new Promise(resolve => setTimeout(resolve, delay));
-        navItems[i].style.transform = "scale(1)";
-    }
+  for (let i = 0; i < navItems.length; i++) {
+    await new Promise((resolve) => setTimeout(resolve, delay));
+    navItems[i].style.transform = "scale(1)";
+  }
+
+  landing.style.transform = "scale(1)";
+  footer.style.transform = "width(100%)";
 };
