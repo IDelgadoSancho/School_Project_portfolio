5fa7261f2670285af7f6bd463401efd600aeb3f1
Ismael Delgado Sancho <ismael_delgadosancho@iescarlesvallbona.cat>
2024-11-14 20:26:33 +0100

    cursor_v1

--- js/code.js
+++ js/code.js
@@ -1,9 +1,23 @@
 window.onload = async function () {
-    let navItems = document.querySelectorAll(".nav ul li");
-    let delay = 320; // 200ms de retardo entre cada elemento
+  // animation landing page
+  let navItems = document.querySelectorAll(".nav ul li");
+  let delay = 320; // 200ms de retardo entre cada elemento
 
-    for (let i = 0; i < navItems.length; i++) {
-        await new Promise(resolve => setTimeout(resolve, delay));
-        navItems[i].style.transform = "scale(1)";
-    }
+  for (let i = 0; i < navItems.length; i++) {
+    await new Promise((resolve) => setTimeout(resolve, delay));
+    navItems[i].style.transform = "scale(1)";
+  }
 };
+
+// cursor
+let cursor = document.querySelector(".cursor-pointer");
+let cursorOutline = document.querySelector(".cursor-outline");
+let act = false;
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.top = e.pageY + "px";
+  cursor.style.left = e.pageX + "px";
+  cursorOutline.style.top = e.pageY + "px";
+  cursorOutline.style.left = e.pageX + "px";
+});
+
