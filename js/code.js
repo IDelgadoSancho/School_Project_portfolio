6b37c01b80aea32da749d8d7c981d12ee521bdbd
Ismael Delgado Sancho <ismael_delgadosancho@iescarlesvallbona.cat>
2024-11-19 18:04:10 +0100

    landing_v2

--- js/code.js
+++ js/code.js
@@ -10,5 +10,68 @@ window.onload = async function () {
   }
 
   landing.style.transform = "scale(1)";
-  footer.style.transform = "width(100%)";
+
+  if (window.innerWidth <= 570) {
+    footer.style.transform = "translateX(0%)";
+    document.getElementById("map_container").style.display = "none";
+  } else {
+    footer.style.transform = "translateX(10%)";
+  }
+  document.getElementById("data_div").onload = showTime();
+  document.getElementById("clock_div").onload = showTime();
+
+  setInterval(showTime, 1000);
+  showTime();
+  showMap();
 };
+
+function showTime() {
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  let hora = "AM";
+
+  if (h == 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    hora = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  let data = `${day}/${month}/${year}`;
+  let time = `${h}:${m}:${s} ${hora}`;
+
+  document.getElementById("data_div").innerText = data;
+  document.getElementById("clock_div").innerText = time;
+  setTimeout(showTime, 1000);
+}
+
+function showMap() {
+  navigator.geolocation.getCurrentPosition(map);
+  function map(position) {
+    let latt = position.coords.latitude;
+    let long = position.coords.longitude;
+    var map = L.map("map_container").setView([latt, long], 10);
+    L.tileLayer(
+      "https://tile.jawg.io/jawg-dark/{z}/{x}/{y}{r}.png?access-token={accessToken}",
+      {
+        // attribution: '<a href="https://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        minZoom: 0,
+        maxZoom: 15,
+        accessToken: "<your accessToken>",
+      }
+    ).addTo(map);
+    // L.control.scale().addTo(map);
+    L.marker([latt, long], { draggable: false }).addTo(map);
+  }
+}
